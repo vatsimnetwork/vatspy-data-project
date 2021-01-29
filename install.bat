8ed8563a0603e56e8180505c1f0662598da3ff77
ewhitaker-zhu <71858269+ewhitaker-zhu@users.noreply.github.com>
2021-01-29 17:23:40 -0600

    Delete install.bat

--- install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-copy FIRBoundaries.dat %AppData%\VAT-Spy
-copy VATSpy.dat %AppData%\VAT-Spy
-echo Data files installed successfully!
-echo Press any key to close this window.
-pause > nul

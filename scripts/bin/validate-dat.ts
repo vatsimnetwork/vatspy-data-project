ca104b7091befae027d27dfe607d1c39ef7b8a82
daniluk4000 <daniluk4000@outlook.com>
2025-08-04 12:22:04 +0300

    Add duplicate validations

--- scripts/bin/validate-dat.ts
+++ scripts/bin/validate-dat.ts
@@ -57,35 +57,66 @@ const parsedDat = parseDatFile({
     dat,
 });
 
+const counters: Record<string, Record<string, true>> = {}
+
+function count(key: string, value: string, throwOnError = true) {
+    counters[key] ??= {}
+    if (counters[key][value]) {
+        if (throwOnError)
+            throw new Error(`Value ${value} in ${key} is duplicated`)
+        else return false
+    }
+
+    counters[key][value] = true
+    return true
+}
+
 for (const country of parsedDat.countries) {
-    if(!country.country || !country.code || country.code.length !== 2) throw new Error(`Country ${country.code} validation failed`);
-    if(country.callsign === undefined) throw new Error(`Country ${country.code} is missing callsign (should be at least empty)`);
+    if (!country.country || !country.code || country.code.length !== 2) throw new Error(`Country ${country.code} validation failed`);
+    if (country.callsign === undefined) throw new Error(`Country ${country.code} is missing callsign (should be at least empty)`);
+
+    count('countries', country.code)
 }
 
 for (const airport of parsedDat.airports) {
-    if(!airport.name || !airport.icao || !airport.fir) throw new Error(`Airport ${airport.icao} validation failed`)
-    if(airport.icao.length !== 4) throw new Error(`Airport ${airport.icao} ICAO should be 4-length`)
-    if(airport.fir.length !== 4) throw new Error(`Airport ${airport.icao} FIR should be 4-length`)
+    if (!airport.name || !airport.icao || !airport.fir) throw new Error(`Airport ${airport.icao} validation failed`)
+    if (airport.icao.length !== 4) throw new Error(`Airport ${airport.icao} ICAO should be 4-length`)
+    if (airport.fir.length !== 4) throw new Error(`Airport ${airport.icao} FIR should be 4-length`)
 
-    if(!airport.lat || isNaN(Number(airport.lat))) throw new Error(`Airport ${airport.icao} lat is invalid`)
-    if(!airport.lon || isNaN(Number(airport.lon))) throw new Error(`Airport ${airport.icao} lon is invalid`)
-    if(airport.isPseudo !== '1' && airport.isPseudo !== '0') throw new Error(`Airport ${airport.isPseudo} should be 0 or 1`)
+    if (!airport.lat || isNaN(Number(airport.lat))) throw new Error(`Airport ${airport.icao} lat is invalid`)
+    if (!airport.lon || isNaN(Number(airport.lon))) throw new Error(`Airport ${airport.icao} lon is invalid`)
+    if (airport.isPseudo !== '1' && airport.isPseudo !== '0') throw new Error(`Airport ${airport.isPseudo} should be 0 or 1`)
+
+    const valid = count('airport-icao', airport.icao, false)
+
+    if (!valid && !airport.isPseudo) {
+        throw new Error(`Airport ${airport.icao} is duplicated`)
+    }
+
+    if (airport.iata)
+        count('airport-iata', airport.iata)
 }
 
 for (const fir of parsedDat.firs) {
-    if(!fir.icao || !fir.name || !fir.boundary) throw new Error(`Fir ${fir.icao} validation failed`)
+    if (!fir.icao || !fir.name || !fir.boundary) throw new Error(`Fir ${fir.icao} validation failed`)
     const boundary = boundaries.features.find((x: any) => x.properties.id === fir.boundary)
-    if(!boundary) throw new Error(`Fir ${fir.icao} boundary was not found`)
+    if (!boundary) throw new Error(`Fir ${fir.icao} boundary was not found`)
+
+    if(fir.callsign)
+    count('fir-callsign-and-icao', fir.callsign + fir.icao)
+    else count('fir-icao', fir.icao)
 }
 
 for (const fir of parsedDat.uirs) {
-    if(!fir.icao || !fir.name || !fir.firs) throw new Error(`UIR ${fir.icao} validation failed`)
+    if (!fir.icao || !fir.name || !fir.firs) throw new Error(`UIR ${fir.icao} validation failed`)
     const firs = fir.firs.split(',')
     const missing = firs.filter(x => !parsedDat.firs.some(y => y.icao === x))
-    if(missing.length) throw new Error(`${missing} for UIR ${fir.icao} missing in FIRs`)
+    if (missing.length) throw new Error(`${missing} for UIR ${fir.icao} missing in FIRs`)
+
+    count('uir', fir.icao)
 }
 
 for (const idl of parsedDat.idl) {
-    if(!idl.first || !idl.last) throw new Error(`IDL validation failed`)
-    if(isNaN(Number(idl.first)) || isNaN(Number(idl.last))) throw new Error(`IDL coordinates validation failed`)
+    if (!idl.first || !idl.last) throw new Error(`IDL validation failed`)
+    if (isNaN(Number(idl.first)) || isNaN(Number(idl.last))) throw new Error(`IDL coordinates validation failed`)
 }
